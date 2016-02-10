f144f265658c79ab6d6d7a7cf7e267523afe183d
gmenajue <gmenajue@alumnos.unex.es>
2016-02-10 17:54:48 +0100

    Primer Commit Vision Artificial

---
+++ mainwindow.cpp
@@ -0,0 +1,251 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    numFoto=0;
+    cap = new VideoCapture(0);
+    if(!cap->isOpened())
+        cap = new VideoCapture(1);
+    capture = true;
+    showColorImage = false;
+    winSelected = false;
+    cap->set(CV_CAP_PROP_FRAME_WIDTH, 320);
+    cap->set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+    imgS = new QImage(320,240, QImage::Format_RGB888);
+    visorS = new RCDraw(320,240, imgS, ui->imageFrameS);
+    imgD = new QImage(320,240, QImage::Format_RGB888);
+    visorD = new RCDraw(320,240, imgD, ui->imageFrameD);
+
+    colorImage.create(240,320,CV_8UC3);
+    grayImage.create(240,320,CV_8UC1);
+    destColorImage.create(240,320,CV_8UC3);
+    destGrayImage.create(240,320,CV_8UC1);
+    gray2ColorImage.create(240,320,CV_8UC3);
+    destGray2ColorImage.create(240,320,CV_8UC3);
+
+    connect(&timer,SIGNAL(timeout()),this,SLOT(compute()));
+    connect(ui->captureButton,SIGNAL(clicked(bool)),this,SLOT(start_stop_capture(bool)));
+    connect(ui->colorButton,SIGNAL(clicked(bool)),this,SLOT(change_color_gray(bool)));
+    connect(ui->ImageButton,SIGNAL(clicked()),this,SLOT(set_Image()));
+    connect(ui->SaveButton,SIGNAL(clicked()),this,SLOT(save_Image()));
+    connect(ui->LoadButton,SIGNAL(clicked()),this,SLOT(load_Image()));
+    connect(ui->ResizeButton,SIGNAL(clicked()),this,SLOT(resize_Image()));
+
+    connect(visorS,SIGNAL(windowSelected(QPointF, int, int)),this,SLOT(selectWindow(QPointF, int, int)));
+    connect(visorS,SIGNAL(pressEvent()),this,SLOT(deselectWindow()));
+    timer.start(60);
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete cap;
+    delete visorS;
+    delete visorD;
+    delete imgS;
+    delete imgD;
+
+}
+
+void MainWindow::compute()
+{
+
+    if(capture && cap->isOpened())
+    {
+        *cap >> colorImage;
+
+        cvtColor(colorImage, grayImage, CV_BGR2GRAY);
+        cvtColor(colorImage, colorImage, CV_BGR2RGB);
+
+    }
+
+
+    if(showColorImage)
+    {
+        memcpy(imgS->bits(), colorImage.data , 320*240*3*sizeof(uchar));
+        memcpy(imgD->bits(), destColorImage.data , 320*240*3*sizeof(uchar));
+    }
+    else
+    {
+        cvtColor(grayImage,gray2ColorImage, CV_GRAY2RGB);
+        cvtColor(destGrayImage,destGray2ColorImage, CV_GRAY2RGB);
+        memcpy(imgS->bits(), gray2ColorImage.data , 320*240*3*sizeof(uchar)); //Pasa el contenido a VisorS
+        memcpy(imgD->bits(), destGray2ColorImage.data , 320*240*3*sizeof(uchar));//Pasa el contenido a VisorD
+
+    }
+
+
+    if(winSelected)
+    {
+        visorS->drawSquare(QPointF(imageWindow.x+imageWindow.width/2, imageWindow.y+imageWindow.height/2), imageWindow.width,imageWindow.height, Qt::green );
+
+    }
+    visorS->update();
+    visorD->update();
+
+
+
+
+}
+void MainWindow::start_stop_capture(bool start)
+{
+    if(start)
+    {
+        ui->captureButton->setText("Pulsar para Parar");
+        capture=true;
+            }
+    else
+    {
+        ui->captureButton->setText("Pulsar Para Capturar");
+        capture = false;
+    }
+}
+void MainWindow::set_Image()
+{
+ if(winSelected){
+     ui->textEdit->setText("Hola");
+//colorImage.copyTo();
+
+   //  destColorImage.resize(colorImage.rowRange(imageWindow.x,imageWindow.width).colRange(imageWindow.y,imageWindow.height));
+
+
+    int centro_wdt_image=imageWindow.width/2;
+    int centro_hgt_image=imageWindow.height/2;
+    //ui->textEdit->setText(centro_hgt_image);
+    printf("Altura : %d",centro_hgt_image);
+
+    int origen_x =160-centro_wdt_image;
+    int origen_y =120-centro_hgt_image;
+
+
+    int fin_x =origen_x+imageWindow.width;
+    int fin_y =origen_y+imageWindow.height;
+
+
+    Mat cuadroImagen=colorImage.colRange(imageWindow.x,imageWindow.x+imageWindow.width).rowRange(imageWindow.y,imageWindow.y+imageWindow.height);
+
+    cuadroImagen.copyTo(destColorImage.colRange(origen_x,fin_x).rowRange(origen_y,fin_y));
+
+
+ }
+ else{
+     destColorImage=colorImage.clone();
+     destGrayImage=grayImage.clone();
+ }
+
+}
+void MainWindow::save_Image()
+{
+ savebool=true;
+ ui->textEdit->setText("Save Image");
+
+ QString filename=QFileDialog::getSaveFileName(0,"Save file",QDir::currentPath(),
+"JPG files (*.jpg);;BMP files (*.bmp);;PNG files (*.png);;All files (*.*)",
+     new QString("Text files (*.txt)"));
+
+ if(showColorImage){
+    cvtColor(colorImage, colorImage, CV_BGR2RGB); //
+    imwrite(filename.toStdString(), colorImage);
+ }
+ else{
+    imwrite(filename.toStdString(), grayImage);
+ }
+//capture=false;
+
+}
+
+
+
+void MainWindow::load_Image()
+{
+
+loadbool=true;
+ui->textEdit->setText("Load Image");
+
+QString filters("JPG files (*.jpg);;BMP files (*.bmp);;PNG files (*.png);;All files (*.*)");
+QString defaultFilter("All files (*.*)");
+
+QFileDialog fileDialog(0, "Open file", "/home/guille/Escritorio", filters);
+fileDialog.selectNameFilter(defaultFilter);
+QStringList fileNames;
+if (fileDialog.exec())
+    fileNames = fileDialog.selectedFiles();
+
+QString File=fileNames.front();
+fileNames.pop_front();
+colorImage=imread(File.toStdString());
+
+//cv::resize(colorImage,colorImage, Size(320,240));
+
+cvtColor(colorImage, grayImage, CV_BGR2GRAY);
+cvtColor(colorImage,colorImage, CV_BGR2RGB);
+
+start_stop_capture(false);
+
+
+
+
+}
+void MainWindow::resize_Image()
+{
+ resizebool=true;
+ ui->textEdit->setText("Resize Image");
+
+
+
+
+}
+
+
+void MainWindow::change_color_gray(bool color)
+{
+    if(color)
+    {
+        ui->colorButton->setText("Gray image");
+        showColorImage = true;
+    }
+    else
+    {
+        ui->colorButton->setText("Color image");
+        showColorImage = false;
+    }
+}
+
+void MainWindow::selectWindow(QPointF p, int w, int h)
+{
+    QPointF pEnd;
+    if(w>0 && h>0)
+    {
+        imageWindow.x = p.x()-w/2;
+        if(imageWindow.x<0)
+            imageWindow.x = 0;
+        imageWindow.y = p.y()-h/2;
+        if(imageWindow.y<0)
+            imageWindow.y = 0;
+        pEnd.setX(p.x()+w/2);
+        if(pEnd.x()>=320)
+            pEnd.setX(319);
+        pEnd.setY(p.y()+h/2);
+        if(pEnd.y()>=240)
+            pEnd.setY(239);
+        imageWindow.width = pEnd.x()-imageWindow.x;
+        imageWindow.height = pEnd.y()-imageWindow.y;
+
+        winSelected = true;
+
+
+    }
+}
+
+void MainWindow::deselectWindow()
+{
+    winSelected = false;
+}
+
+
---
+++ mainwindow.h
@@ -0,0 +1,62 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/features2d/features2d.hpp>
+//#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
+#include <rcdraw.h>
+
+
+
+using namespace cv;
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    int numFoto;
+    QTimer timer;
+
+    VideoCapture *cap;
+    RCDraw *visorS, *visorD;
+    QImage *imgS, *imgD;
+    Mat colorImage, grayImage, destColorImage, destGrayImage;
+    Mat gray2ColorImage, destGray2ColorImage;
+    bool capture, showColorImage, winSelected;
+    bool click, savebool, loadbool,resizebool;
+    Rect imageWindow;
+
+
+public slots:
+    void compute();
+    void start_stop_capture(bool start);
+    void change_color_gray(bool color);
+    void set_Image();
+    void save_Image();
+    void load_Image();
+    void resize_Image();
+
+    void selectWindow(QPointF p, int w, int h);
+    void deselectWindow();
+
+};
+
+
+#endif // MAINWINDOW_H
